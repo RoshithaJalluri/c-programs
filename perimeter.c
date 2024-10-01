37907f27ea3577ca565ddec8b2189df39df7c806
RoshithaJalluri <22mq1a4215@sviet.edu.in>
2024-10-01 02:21:40 -0700

    Create perimeter.c

---
+++ perimeter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int l,b,perimeter;
+    printf("enter the values of l,b:");
+    scanf("%d%d",&l,&b);
+    perimeter=2*(l+b);
+    printf("the result is %d",perimeter);
+}
