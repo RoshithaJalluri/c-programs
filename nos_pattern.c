c846a0b29ac542e2eed39e10822dbb403e5ec230
RoshithaJalluri <22mq1a4215@sviet.edu.in>
2024-12-30 11:29:09 +0530

    Update nos_pattern.c

--- nos_pattern.c
+++ nos_pattern.c
@@ -12,3 +12,20 @@ int main()
     }
     return 0;
 }
+
+
+#include<stdio.h>
+int main()
+{
+    int i,j,n=1;
+    for(i=0;i<5;++i)
+    {
+        for(j=0;j<=i;++j)
+        {
+            printf("%d\t",n);
+        }
+        n++;
+        printf("\n");
+    }
+    return 0;
+}
