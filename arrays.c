61d01c5b2519c395d00f89867ab3b812cc7b8756
RoshithaJalluri <22mq1a4215@sviet.edu.in>
2025-01-09 18:51:05 +0530

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
+#array sample program
 
+#include <stdio.h>
 int main()
 {
     int a[10],n,i;
@@ -15,3 +16,127 @@ int main()
         printf("%d\t",a[i]);
     }
 }
+
+#AVG OF ARRAYS
+
+int main()
+{
+    int a[10],n,i,sum=0,avg=0;
+    printf("Enter size: ");
+    scanf("%d",&n);
+    printf("Enter list of elements: ");
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;++i)
+    {
+        sum=sum+a[i];
+    }
+    avg=sum/2;
+    printf("The average of elements of array %d",avg);
+    return 0;
+}
+
+#SUM OF ARRAYS
+#include <stdio.h>
+
+int main()
+{
+    int a[10],n,i,sum=0;
+    printf("Enter size: ");
+    scanf("%d",&n);
+    printf("Enter list of elements: ");
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;++i)
+    {
+        sum=sum+a[i];
+    }
+    printf("The sum of elements of array %d",sum);
+    return 0;
+}
+
+#MAX VALUE OF ARRAYS
+
+#include <stdio.h>
+
+int main()
+{
+    int a[10],n,i,max;
+    printf("Enter size: ");
+    scanf("%d",&n);
+    printf("Enter list of elements: ");
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+    for(i=0;i<n;++i)
+    {
+       if(a[i]>max)
+       {
+           max=a[i];
+       }
+    }
+    printf("The max value of elements of array %d",max);
+    return 0;
+}
+
+#MIN VALUE OF ARRAY
+
+#include <stdio.h>
+
+int main()
+{
+    int a[10],n,i,min;
+    printf("Enter size: ");
+    scanf("%d",&n);
+    printf("Enter list of elements: ");
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",&a[i]);
+    }
+    min=a[0];
+    for(i=0;i<n;++i)
+    {
+       if(a[i]<min)
+       {
+           min=a[i];
+       }
+    }
+    printf("The min value of elements of array %d",min);
+    return 0;
+}
+
+#DUPLICATES IN ARRAY
+
+#include <stdio.h>
+
+int main()
+{
+    int a[10],n,i,j,count=0;
+    printf("Enter size: ");
+    scanf("%d",&n);
+    printf("Enter list of elements: ");
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;++i)
+    {
+        for(j=i+1;j<n;++j)
+         {
+           if(a[i]==a[j])
+             {
+               count++;
+               break;
+              }
+         }
+    }
+    printf("Total no of duplicates in array %d",count);
+    return 0;
+}
+
