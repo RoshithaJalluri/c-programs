8bb1ccb195e4e93bf47fc82df7a271c84acd18a0
RoshithaJalluri <22mq1a4215@sviet.edu.in>
2025-01-19 20:26:24 +0530

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -208,6 +208,38 @@ int main()
 }
 
 #SEPARATE EVEN AND ODD NOS IN 2 SEPARATE ARRAYS
-
+int main()
+{
+    int a[10],b[10],c[10],n,i,j,k;
+    printf("Enter size: ");
+    scanf("%d",&n);
+    printf("Enter the elements: ");
+    for(i=0;i<n;++i)
+    {
+     scanf("%d",&a[i]);   
+    }
+    for(i=0;i<n;++i)
+    {
+        if(a[i]%2==0)
+        {
+             b[j]=a[i];
+             j++;
+        }
+        else
+        {
+            c[k]=a[i];
+            k++;
+        }
+    }
+    for(i=0;i<j;++i)
+    {
+        printf("%d",b[i]);
+    }
+    for(i=0;i<k;++i)
+    {
+        printf("%d",c[i]);
+    }
+    return 0;
+}
 
 
