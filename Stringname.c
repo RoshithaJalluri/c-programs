9b1f63809002921eabddf2d056899212656e65fe
RoshithaJalluri <22mq1a4215@sviet.edu.in>
2025-01-20 19:00:54 +0530

    Create Stringname.c

---
+++ Stringname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    char str[100],s1;
+    printf("Enter size: ");
+    scanf("%d",&s1);
+    printf("Enter string name: ");
+    scanf("%s",str);
+    printf("your name is %s",str);
+    return 0;
+}
